2c4ee6dba1312f24f1c6fddbf33ac01111792e3b
isdsys <hideishi777@gmail.com>
2018-05-03 20:51:57 +0900

    aaa

--- jp_001/src/app/AppMain.java
+++ jp_001/src/app/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Git World !!!");
+		System.out.println("Hello Git World 123 !!!");
 	}
 
 }
